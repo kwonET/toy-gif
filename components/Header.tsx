8a6133ff34b9c6fd78f6996f60539bab8a7135db
Bomi Kwon <49463954+kwonET@users.noreply.github.com>
2022-07-18 17:02:14 +0900

    add [determine structure]
    #1
    - 헤더 및 메인 파트 구분
    - 기본 UI 틀 잡기
    - 카테고리 추가

---
+++ components/Header.tsx
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+import { CtgrItm } from "./items/CtgrItem";
+import { LineItm } from "./items/LineItem";
+
+export const Header = ({ cdata }) => {
+  return (
+    <HeaderWrapper>
+      
+      <CenterWrapper>
+        <TitleWrapper>
+          <div className="text">
+          짤생성기
+          </div>
+          </TitleWrapper>
+        <LineItm></LineItm>
+        <CtgrListWrapper>
+          {cdata.map((item) => {
+            return <CtgrItm key={item.key} ctgrName={item} />;
+          })}
+        </CtgrListWrapper>
+        
+      </CenterWrapper>
+    </HeaderWrapper>
+  );
+};
+const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const TitleWrapper = styled.div`
+  /* 짤방생성기 */
+
+  /* position: absolute; */
+  width: 1200px;
+  height: 104px;
+  gap: 32px;
+  /* left: 40px;
+  top: 56px; */
+
+  font-family: "Noto Sans CJK KR";
+  font-style: normal;
+  font-weight: 900;
+  font-size: 72px;
+  line-height: 104px;
+
+  color: #000000;
+  & > .text{
+    display: flex;
+    
+  }
+`;
+const CtgrListWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 20px;
+`;
+
+const CenterWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  width: 1200px;
+  height: 200px;
+`;
